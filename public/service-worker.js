abe8886bc27a7b7ffc8585a29821bf6a421280fa
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2022-01-20 21:01:49 -0700

    fixed paths for images and css

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,15 +6,15 @@ const FILES_TO_CACHE = [
     '/',
     '/index.html',
     '/manifest.json',
-    '/assets/css/style.css',
-    '/assets/images/icons/icon-72x72.png',
-    '/assets/images/icons/icon-96x96.png',
-    '/assets/images/icons/icon-128x128.png',
-    '/assets/images/icons/icon-144x144.png',
-    '/assets/images/icons/icon-152x152.png',
-    '/assets/images/icons/icon-192x192.png',
-    '/assets/images/icons/icon-384x384.png',
-    '/assets/images/icons/icon-512x512.png'
+    '/public/css/style.css',
+    '/public/icons/icon-72x72.png',
+    '/public/icons/icon-96x96.png',
+    '/public/icons/icon-128x128.png',
+    '/public/icons/icon-144x144.png',
+    '/public/icons/icon-152x152.png',
+    '/public/icons/icon-192x192.png',
+    '/public/icons/icon-384x384.png',
+    '/public/icons/icon-512x512.png'
 ];
 
 //install service worker 
