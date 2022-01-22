d591f2c87e88578d8e2814cbfec30af818bb6225
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2022-01-22 12:09:02 -0700

    fixed problems and PWA now working

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,15 +5,15 @@ const FILES_TO_CACHE = [
     '/',
     '/index.html',
     '/manifest.json',
-    '/public/css/style.css',
-    '/public/icons/icon-72x72.png',
-    '/public/icons/icon-96x96.png',
-    '/public/icons/icon-128x128.png',
-    '/public/icons/icon-144x144.png',
-    '/public/icons/icon-152x152.png',
-    '/public/icons/icon-192x192.png',
-    '/public/icons/icon-384x384.png',
-    '/public/icons/icon-512x512.png'
+    'css/styles.css',
+    '/icons/icon-72x72.png',
+    '/icons/icon-96x96.png',
+    '/icons/icon-128x128.png',
+    '/icons/icon-144x144.png',
+    '/icons/icon-152x152.png',
+    '/icons/icon-192x192.png',
+    '/icons/icon-384x384.png',
+    '/icons/icon-512x512.png'
 ];
 
 //install service worker 
@@ -21,9 +21,10 @@ self.addEventListener('install', function(e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
             console.log('installing cache: ' + CACHE_NAME)
-            return cache.addAll(FILES_TO_CACHE)
+            return cache.addAll(FILES_TO_CACHE);
         })
     )
+    self.skipWaiting();
 })
 
 self.addEventListener('activate', function(e) {
@@ -31,7 +32,7 @@ self.addEventListener('activate', function(e) {
         caches.keys().then(keyList =>{
             return Promise.all(
                 keyList.map(key => {
-                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                    if (key !== CACHE_NAME && key !== VERSION) {
                         console.log('removing cache data', key);
                         return caches.delete(key);
                     }
@@ -44,7 +45,7 @@ self.addEventListener('activate', function(e) {
 })
 
 //intercept fetch request 
-self.addEventListener('fetch', (evt) => {
+self.addEventListener('fetch', (e) => {
     if (e.request.url.includes('/api/')) {
         e.respondWith(
             caches
